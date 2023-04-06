c9292b079a2589941c20c3186dd4f2484a9b625e
Isi-Te <isabell.teuber@gmail.com>
2023-04-06 10:11:03 -0400

    Add login post and validation.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,9 @@
 const knex = require('knex')(require('../knexfile'));
 const jwt = require('jsonwebtoken');
 const { v4: uuid } = require('uuid');
+const bcrypt = require('bcrypt');
 
-const secretKey = "58yuht4jrgkv9sdf8uht";
+const JWT_SECRET = "58yuht4jrgkv9sdf8uht";
 
 exports.getUser = (req, res) => {
     knex('user')
@@ -14,40 +15,48 @@ exports.getUser = (req, res) => {
         );
 };
 
+
 exports.userLogin = (req, res) => {
+    const { username, password } = req.body;
 
-    const users = getUser();
+    knex('user')
+        .where({ username })
+        .first()
+        .then((user) => {
+            if (!user) {
+                res.status(404).json({
+                    error: 'Username not found.'
+                });
+                return;
+            }
 
-    const name = req.body.name;
-    const password = req.body.password;
+            if (password !== user.password) {
+                res.status(401).json({
+                    error: 'Invalid password.'
+                });
+                return;
+            }
 
-    const Login = { id: uuid(), ...req.body }
-
-    const user = users.find(user => user.name === name);
-
-    if (user) {
-        // Check if the password matches
-        if (user.password === password) {
-            // Login successful
-            const token = jwt.sign({ name: name }, secretKey);
-            res.json({
-                token: token
-            });
-        } else {
-            // Incorrect password
-            res.status(401);
-            res.json({
-                error: "Password is incorrect."
-            });
-        }
-    } else {
-        // Username doesn't exist in our list of users
-        res.status(404);
-        res.json({
-            error: "User not found"
+            const token = jwt.sign({ username: user.username }, JWT_SECRET);
+            res.status(200).json({ token });
+        })
+        .catch((err) => {
+            console.error(`Error retrieving user: ${err}`);
+            res.status(500).json({ error: 'Internal server error.' });
         });
-    }
-}
+};
+
+// else {
+//     knex('user')
+//         .where({ username: username, password: password })
+//         .then(() => {
+//             let token = jwt.sign({ username: username })
+//             res.status(201) / json({ token: token });
+//         })
+//         .catch((err) =>
+//             res.status(403).send({ token: null })
+//         );
+// }
 
 // knex('user')
 //     .insert(userLogin)
@@ -56,3 +65,30 @@ exports.userLogin = (req, res) => {
 //     })
 //     .catch((error) => res.status(400).send(`Error creating Journal Entry: ${error}`));
 
+// exports.userLogin = (req, res) => {
+//     const { username, password } = req.body;
+
+//     knex('user')
+//         .where({ username })
+//         .then((data) => {
+//             if (data.length === 0) {
+//                 res.status(404);
+//                 res.json({
+//                     error: "User not found"
+//                 });
+//             } if (password !== data[0].password) {
+//                 res.status(401);
+//                 res.json({
+//                     error: "Password is incorrect."
+//                 });
+//             }
+
+//             let token = jwt.sign({ username });
+//             res.status(201).json({ token });
+
+//         })
+//         .catch((err) => {
+//             console.error(`Error retrieving user: ${err}`);
+//             res.status(500).json({ error: "Internal server error." });
+//         });
+// }
